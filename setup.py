f121bf91a89f08ffa9075cdb41ee9e66de91115a
Eero af heurlin <eero.afheurlin@iki.fi>
2014-09-01 12:41:13 +0300

    fix typos in the requirements spec

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ setup(
     long_description=open('README.md').read(),
     install_requires=[
         'tornado>=2.0',
-        'pyzqm=>2.0',
-        'pybonjour=>1.1',
+        'pyzmq>=2.0',
+        'pybonjour>=1.1',
     ],
     url='https://github.com/rambo/python-zmqdecorators',
 )
